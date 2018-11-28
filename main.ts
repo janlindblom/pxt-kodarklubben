13c3f32a7507edf3ad67d19e35692f58fb1b3cf3
Jan Lindblom <janlindblom@fastmail.fm>
2018-11-28 20:12:50 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,63 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf2db" block="Kodarklubben"
+namespace kodarklubben {
+
+    //% blockId=kodarklubben_spiral
+    //% block="spiral"
+    export function spiral() {
+        let y = 0
+        let x = 0
+        led.stopAnimation()
+        basic.clearScreen()
+        y = 0
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(20)
+        }
+        x = 4
+        for (let y = 0; y <= 4; y++) {
+            led.plot(x, y)
+            basic.pause(20)
+        }
+        y = 4
+        for (let x = 0; x <= 4; x++) {
+            led.plot(4 - x, y)
+            basic.pause(20)
+        }
+        x = 0
+        for (let y = 0; y <= 3; y++) {
+            led.plot(x, 4 - y)
+            basic.pause(20)
+        }
+        y = 1
+        for (let x = 0; x <= 3; x++) {
+            led.plot(x, y)
+            basic.pause(20)
+        }
+        x = 3
+        for (let y = 0; y <= 3; y++) {
+            led.plot(x, y)
+            basic.pause(20)
+        }
+        y = 3
+        for (let x = 0; x <= 3; x++) {
+            led.plot(4 - x, y)
+            basic.pause(20)
+        }
+        x = 1
+        y = 2
+        led.plot(x, y)
+        basic.pause(20)
+        x = 2
+        y = 2
+        led.plot(x, y)
+    }
+
+    //% blockId=kodarklubben_name
+    //% block="kodarklubben"
+    export function kodarklubben() {
+        basic.showString("Kodarklubben r.f.")
+    }
+}
